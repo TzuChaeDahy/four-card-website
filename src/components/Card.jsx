5ae7f3484fda6eaea15496a0b568178a72956c2c
TzuChaeDahy <viniciuslovestwice@gmail.com>
2022-10-27 19:32:33 -0300

    solving a border issue pt.2

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -35,7 +35,7 @@ function Card({ title, children, cardKey, color }) {
       <img
         src={sources[cardKey]}
         alt="No value"
-        className="mt-5 sm:mt-10 ml-auto border"
+        className="mt-5 sm:mt-10 ml-auto"
       />
     </li>
   );
